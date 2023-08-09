77f327a650bc14f5670e75b44c56dc72bbc27e21
Maria Frota <mariacarolina.mf@gmail.com>
2023-08-09 01:25:00 -0300

    Data e Network - Parte 2

---
+++ Domain/DataProvider/GeneticDataProvider.swift
@@ -0,0 +1,8 @@
+//
+//  GeneticDataProvider.swift
+//  MarketCoins
+//
+//  Created by Maria on 21/07/23.
+//
+
+import Foundation
---
+++ Domain/Models/GlobalModel.swift
@@ -0,0 +1,40 @@
+//
+//  GlobalModel.swift
+//  MarketCoins
+//
+//  Created by Maria on 21/07/23.
+//
+
+import Foundation
+
+// MARK: - CoinModel
+struct GlobalModel: Codable {
+    let data: CryptoCurrencyModel
+}
+
+// MARK: - DataClass
+struct CryptoCurrencyModel: Codable {
+    let activeCryptocurrencies: Int
+    let upcomingIcos: Int
+    let ongoingIcos: Int
+    let endedIcos: Int
+    let markets: Int
+    let totalMarketCap: [String: Double]
+    let totalVolume: [String: Double]
+    let marketCapPercentage: [String: Double]
+    let marketCapChangePercentage24HUsd: Double
+    let updatedAt: Int
+
+    enum CodingKeys: String, CodingKey {
+        case activeCryptocurrencies = "active_cryptocurrencies"
+        case upcomingIcos = "upcoming_icos"
+        case ongoingIcos = "ongoing_icos"
+        case endedIcos = "ended_icos"
+        case markets
+        case totalMarketCap = "total_market_cap"
+        case totalVolume = "total_volume"
+        case marketCapPercentage = "market_cap_percentage"
+        case marketCapChangePercentage24HUsd = "market_cap_change_percentage_24h_usd"
+        case updatedAt = "updated_at"
+    }
+}
